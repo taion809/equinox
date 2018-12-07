247ed2ac9517149814960791b33277b2d9943046
Ruben Bartelink <ruben@bartelink.com>
2018-12-07 09:58:44 +0000

    todos todo wip

---
+++ samples/TodoBackend/Controllers/TodosController.fs
@@ -0,0 +1,43 @@
+﻿namespace TodoBackend.Controllers
+
+open Microsoft.AspNetCore.Http.Extensions // GetEncodedUrl
+open Microsoft.AspNetCore.Mvc
+open TodoBackend
+
+type TodoView = { id: int; url: string }
+
+// Stolen from https://github.com/ChristianAlexander/dotnetcore-todo-webapi/blob/master/src/TodoWebApi/Controllers/TodosController.cs
+// even more stolen from https://github.com/joeaudette/playground/blob/master/spa-stack/src/FSharp.WebLib/Controllers.fs
+[<Route "[controller]">]
+type TodosController(service: Service) =
+    inherit Controller()
+
+    member private __.WithUri(x : Todo, ?id) : TodoView =
+        let self = __.Request.GetEncodedUrl()
+        { id = x.id; url = match id with None -> self | Some id -> sprintf "%s/%O" self id }
+
+    [<HttpGet>]
+    member __.Get() : Async<TodoView seq> = async {
+        let! xs = service.List
+        return seq { for x in xs -> __.WithUri(x, x.id) }
+    }
+
+    [<HttpGet"{id}">]
+    member __.Get id : Async<IActionResult> = async {
+        let! x = service.TryGet id
+        return match x with None -> __.NotFound() :> _ | Some x -> ObjectResult(__.WithUri x) :> _
+    }
+
+    [<HttpPost>]
+    member __.Post([<FromBody>]todo) : Async<TodoView> = async {
+        do! service.Execute <| Add todo
+        return __.WithUri todo
+    }
+
+    [<HttpDelete "{id}">]
+    member __.Delete id : Async<unit> =
+        service.Execute <| Delete id
+
+    [<HttpDelete>]
+    member __.Clear() : Async<unit> =
+        service.Execute <| Clear
---
+++ samples/TodoBackend/TodosService.fs
@@ -0,0 +1,54 @@
+﻿namespace TodoBackend
+
+// NB - these schemas reflect the actual storage formats and hence need to be versioned with care
+[<AutoOpen>]
+module Events =
+    type Todo = { id: int }
+    type Event =
+        | Added                                 of Todo
+        | Deleted                               of int
+        | Cleared                               of unit
+        | Compacted                             of Todo[]
+        interface TypeShape.UnionContract.IUnionContract
+
+module Folds =
+    type State = Todo list
+    let initial = []
+    let evolve s e =
+        match e with
+        | Added item -> item :: s
+        | Deleted id -> s |> List.filter (fun x -> x.id <> id)
+        | Cleared () -> initial
+        | Compacted items -> List.ofArray items
+    let fold state = Seq.fold evolve state
+    let isOrigin = function Events.Compacted _ -> true | _ -> false
+    let snapshot = isOrigin, fun state -> Compacted (Array.ofList state)
+
+type Command = Add of Todo | Delete of id: int | Clear
+
+module Commands =
+    let interpret c _state =
+        match c with
+        | Add item -> [Added item]
+        | Delete id -> [Deleted id]
+        | Clear -> [Cleared ()]
+
+type Handler(log, stream, ?maxAttempts) =
+    let inner = Equinox.Handler(Folds.fold, log, stream, maxAttempts = defaultArg maxAttempts 2)
+    member __.Execute command : Async<unit> =
+        inner.Decide <| fun ctx ->
+            ctx.Execute (Commands.interpret command)
+    member __.Query(projection : Folds.State -> 't) : Async<'t> =
+        inner.Query projection
+
+type Service(handlerLog, resolve) =
+    let stream = Handler(handlerLog, resolve (Equinox.CatId("Todos", "1")))
+
+    member __.List : Async<Todo seq> =
+        stream.Query Seq.ofList
+
+    member __.TryGet id =
+        stream.Query (List.tryFind (fun x -> x.id = id))
+
+    member __.Execute command : Async<unit> =
+        stream.Execute command
